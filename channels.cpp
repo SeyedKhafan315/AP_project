4e1ac00baf2bc4d0cc7d8931369a48d1b03ef08f
Darkensky11-11 <174255301+Darkensky11-11@users.noreply.github.com>
2024-07-11 06:01:23 +0330

    create login and sginup api

--- channels.cpp
+++ channels.cpp
@@ -32,16 +32,48 @@ void Channels::logIn(QString){
     QString Buffer;
     Buffer = QString(socket->readAll());
 
+    std::ifstream inFile("users.bin", std::ios::binary);
 
+    if (!inFile) {
+
+        Player p;
+        while (inFile.read(reinterpret_cast<char*>(&p), sizeof(Player))) {
+            if (QString::fromStdString(p.name) == Buffer) {
+                // Perform login logic here
+                // ...
+                return;
+            }
+        }
+    }
+
+    // send data when cant login
+
+    return;
 }
+
 void Channels::singUp(QString){
-    QString Buffer;
+
     socket->waitForReadyRead(-1);
-    Buffer = QString(socket->readAll());
+    auto Buffer = QString(socket->readAll()).split(',');
 
 
+    string filePath = "users.bin";
 
+    Player players;
+    strcpy(players.name, Buffer[0].toStdString().c_str());
+    strcpy(players.pass, Buffer[1].toStdString().c_str());
+    strcpy(players.email, Buffer[2].toStdString().c_str());
+
+    std::ofstream outFile(filePath, std::ios::binary);
+    if (!outFile) {
+    // std::?cerr << "Error opening file for writing." << std::endl;
+        return;
+    }
+
+    outFile.write(reinterpret_cast<const char*>(&players), sizeof(Player));
+    outFile.close();
 }
+
 void Channels::gameReq(QString){
     QString Buffer;
     socket->waitForReadyRead(-1);
