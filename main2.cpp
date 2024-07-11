236fe56bd3432c993594401b684ed5435f2df97a
Darkensky11-11 <174255301+Darkensky11-11@users.noreply.github.com>
2024-07-11 11:59:34 +0330

    ignore mainwindow

---
+++ main2.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include "server.h"
+
+
+
+// int main(int argc, char *argv[]) {
+//     QCoreApplication a(argc, argv);
+//     qDebug()<< "hello";
+//     server myserver(nullptr);
+
+
+
+//     return a.exec();
+// }
